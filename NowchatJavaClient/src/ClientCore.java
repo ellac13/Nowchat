fbca1e1d8081af46de7d95d95e0e6f6bdc9008ab
ellac13 <calle.13@hotmail.com>
2015-04-16 09:57:38 +0200

    Javaclient string send fix

--- NowchatJavaClient/src/ClientCore.java
+++ NowchatJavaClient/src/ClientCore.java
@@ -1,5 +1,6 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
 
@@ -39,7 +40,7 @@ public class ClientCore {
 			    if (fromServer.equals("Bye."))
 			        break;
 
-			    fromUser = Integer.toString(System.in.read());
+			    fromUser = (new BufferedReader(new InputStreamReader(System.in))).readLine();
 			    if (fromUser != null) {
 			        System.out.println("Client: " + fromUser);
 			        out.println(fromUser);
